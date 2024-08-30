c8277177b510fe1a82032bc54915e66cdaab7e46
Linuka Arambawela <linukaarambawela@gmail.com>
2024-08-30 20:36:22 +0530

    SecurityConfig updated

--- src/main/java/com/ceylonexplorer/CeylonExplorer/config/SecurityConfig.java
+++ src/main/java/com/ceylonexplorer/CeylonExplorer/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Allow the frontend origin
+                .allowedOrigins("https://linukaar.github.io") // Allow the frontend origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow the necessary HTTP methods
                 .allowedHeaders("*")
                 .allowCredentials(true);
